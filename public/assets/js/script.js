b7ffe54ae8d99a5f70915860d85872692d4c11a1
Dennis Timmermann <timmermann.dennis@googlemail.com>
2014-03-12 06:11:36 +0100

    late night coding

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,34 +6,85 @@ $( document ).ready(function() {
         bg.removeClass("office public concert");
     }
 
+    var hide = function(e) {
+        $(e).addClass('hide');
+    }
+
+    var show = function(e) {
+        $(e).removeClass('hide');
+    }
+
+    var makePublic = function() {
+        $(".entry").each(function(i, e) {
+            $(e).find('.picture').addClass('blur');
+            if($(e).data("s").indexOf('public-all') != -1) {
+                //do nottin
+            }
+            else if($(e).data("s").indexOf('public-name') != -1) {
+                $(e).find('.text').addClass('blur');
+            }
+            else {
+                hide(e);
+            }
+        });
+    }
+
+    var makeFamily = function() {
+        $(".entry").each(function(i, e) {
+            $(e).find('.picture').addClass('blur');
+            if($(e).data("s").indexOf('family-all') != -1) {
+                //do nottin
+            }
+            else if($(e).data("s").indexOf('family-name') != -1) {
+                $(e).find('.text').addClass('blur');
+            }
+            else {
+                hide(e);
+            }
+        });
+    }
+
+    var revert = function() {
+        $(".entry").each(function(i, e) {
+            show(e);
+            $(e).find('.picture').removeClass('blur');
+            $(e).find('.text').removeClass('blur');
+        })
+    }
+
     $("#private").bind("click", function(e) {
         removeBg();
         bg.addClass("office")
+        revert();
     });
 
     $("#family").bind("click", function(e) {
         removeBg();
         bg.addClass("concert")
+        revert();
+        makeFamily();
     });
 
     $("#public").bind("click", function(e) {
         removeBg();
         bg.addClass("public")
+        revert();
+        makePublic();
     });
 
-    $(".entry").each(function(i, e) {
-        if($(e).data("s").indexOf('public') != -1) {
-            console.log("yap yap")
-        }
-        //console.log($(e).data("s"));
-    });
+    // $(".entry").each(function(i, e) {
+    //     if($(e).data("s").indexOf('private') != -1) {
+    //         console.log("yap yap")
+    //     }
+    //     else {
+    //         hide(e);
+    //     }
+    //     //console.log($(e).data("s"));
+    // });
 
-    var makePublic = function() {
-        $(".entry").each(function(i, e) {
-            $(e).find("h3").addClass("blur");
-            $(e).find(".picture").addClass("blur");
-        })
-    }
+
+
+
 
     window.makePublic = makePublic;
 
